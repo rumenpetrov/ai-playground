bc45a06ed638445ff019096b40c4a413b3ab72b3
Rumen Petrov <rumen.petrov@blubird.eu>
2024-04-02 09:12:44 +0300

    Fix compose issue and add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import auth from 'auth-astro';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [auth()],
+  integrations: [auth(), react()],
   adapter: node({
     mode: "standalone"
-  }),
+  })
 });
